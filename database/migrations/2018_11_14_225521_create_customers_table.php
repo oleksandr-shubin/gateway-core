b6187b9fc856656af259b2eb612215aa516cda5c
Oleksandr Shubin <oleksandr.shubin@gmail.com>
2018-11-15 01:07:44 +0200

    Init. Company/Customer mcr created

---
+++ database/migrations/2018_11_14_225521_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('given_name');
+            $table->string('family_name');
+            $table->string('email');
+            $table->integer('company_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropForeign(['company_id']);
+        });
+
+        Schema::dropIfExists('customers');
+    }
+}
